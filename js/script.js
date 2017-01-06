0c04f0ebfb5bbc3fd16a2b9a0374216d29977bd4
loracy <lora860227@gmail.com>
2017-01-06 23:50:42 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.social-icon img').mouseenter(function(){
+		$(this).css("background-color","#a6836f");
+		$(this).css("border-radius","30px");
+		});
+	$('.social-icon img').mouseleave(function(){
+		$(this).css("background-color","unset");
+		});
+});
