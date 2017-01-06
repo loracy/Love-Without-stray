910332001bf8cc00129b546f1b97b358e0012ed3
RACY\亞儒 <lora860227@gmail.com>
2017-01-07 07:08:34 +0800

    多了adopt

--- js/script.js
+++ js/script.js
@@ -7,6 +7,43 @@ $(function(){
 		$(this).css("background-color","unset");
 		});
 	$('.btn').click(function(){
-        window.location.href='facebook.com';
+        window.location.href='adopt.html';
 	});
+	$('.step-1').click(function(){
+        window.location.href='#';
+	});
+	$('.step-2').click(function(){
+        window.location.href='adopt.html';
+	});
+	$('.step-3').click(function(){
+        window.location.href='example.html';
+	});
+//------------scroll-down----------------------
+	$('.glyphicon-chevron-down').click(function(){
+		// window.scrollTo(0,800);
+		$('body').animate({
+			scrollTop: 625
+		}, 600);
+	});
+// -------------adopt_dog---------------------
+    $('.dog').mouseenter(function(){
+		$(this).html('<img src="img/dog_hover.jpg">');
+	});
+	$('.dog').mouseleave(function(){
+    	$(this).html('<img src="img/dog.jpg">');
+	});
+	$('.dog').click(function(){
+		window.location.href='adopt_dog.html';
+	});
+// -------------adopt_cat---------------------
+	$('.cat').mouseenter(function(){
+		$(this).html('<img src="img/cat_hover.jpg">');
+	});
+	$('.cat').mouseleave(function(){
+    	$(this).html('<img src="img/cat.jpg">');
+	});
+	$('.cat').click(function(){
+		window.location.href='adopt_cat.html';
+	});
+
 });
