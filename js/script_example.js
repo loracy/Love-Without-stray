b34c34daf10358ebcbb99c81187122c7b503e750
a6z <a6z>
2017-01-09 22:00:29 +0800

    成功案例+換我分享的button，但是沒有連結做好看ㄉ：）

--- js/script_example.js
+++ js/script_example.js
@@ -1,5 +1,8 @@
 $(function(){
-  $('.glyphicon-chevron-down').click(function(){
+  $('.to-sec2').click(function(){
     $('html,body').animate({scrollTop:$('.section2').offset().top}, 800);
+  }); //代表一個完整的執行區塊
+  $('.to-sec3').click(function(){
+    $('html,body').animate({scrollTop:$('.section3').offset().top}, 800);
   }); //代表一個完整的執行區塊
 });
